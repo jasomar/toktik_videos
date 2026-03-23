66d27ac5436d0576b86406e20f0dcfaff1721249
Jafet Solano <jasomar1998@gmail.com>
2026-03-22 18:42:32 -0600

    video provider added and mapper configurated

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/infrastructure/models/local_video_model.dart';
+import 'package:toktik/shared/data/local_video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
   bool initialLoading = true;
@@ -7,6 +9,20 @@ class DiscoverProvider extends ChangeNotifier {
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    final List<VideoPost> newVideos = videoPosts
+        .map(
+          (video) => LocalVideoModel.fromJson(
+            video,
+          ).toVideoPostEntity(),
+        )
+        .toList();
+
+    videos.addAll(newVideos);
+
+    initialLoading = false;
+
     notifyListeners();
   }
 }
