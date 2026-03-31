6c6901afacbd0c07649d38cf7e1913e808a77d85
Jafet Solano <jasomar1998@gmail.com>
2026-03-30 18:34:42 -0600

    package animation added

--- lib/presentation/widgets/shared/video_buttons.dart
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -1,4 +1,6 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:toktik/config/helpers/humans_formats.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 
 class VideoButtons extends StatelessWidget {
@@ -14,9 +16,17 @@ class VideoButtons extends StatelessWidget {
           iconData: Icons.favorite,
           iconColor: Colors.red,
         ),
+        const SizedBox(height: 20),
         _CustomIconButton(
           value: video.views,
           iconData: Icons.remove_red_eye_outlined,
+        ),
+        const SizedBox(height: 20),
+        BounceInDown(
+          child: const _CustomIconButton(
+            value: 0,
+            iconData: Icons.play_circle_outline,
+          ),
         ),
       ],
     );
@@ -42,7 +52,12 @@ class _CustomIconButton extends StatelessWidget {
           onPressed: () {},
           icon: Icon(iconData, color: color, size: 30),
         ),
-        Text('$value'),
+        if (value > 0)
+          Text(
+            HumansFormats.humanReadbleNumber(
+              value.toDouble(),
+            ),
+          ),
       ],
     );
   }
