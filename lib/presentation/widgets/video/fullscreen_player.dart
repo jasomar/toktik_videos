837f161b12e18bd5c0028ec7bba2d8f6b2add0e7
Jafet Solano <jasomar1998@gmail.com>
2026-03-31 18:35:59 -0600

    test cases

---
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FullScreenPlayer extends StatelessWidget {
+  final String videoUrl;
+  final String captios;
+
+  const FullScreenPlayer({
+    super.key,
+    required this.videoUrl,
+    required this.captios,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
